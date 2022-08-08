90424ceb6f6b55be665fd781a363a53b407dae45
Choudhary8055 <80520509+Choudhary8055@users.noreply.github.com>
2022-08-09 01:10:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import Home from './pages/Home';
+import Contect from "./pages/Contect";
+import AboutUs from './pages/AboutUs';
+import Menu from "./pages/Menu";
+import Cart from "./Cart/Cart";
+
+import { BrowserRouter ,Route, Routes } from "react-router-dom";
+  
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <BrowserRouter>
+    
+         <Navbar/>
+        
+     
+          <Routes>
+         
+             <Route path="/cart" element= {<Cart />}/>
+             <Route path="/menu" element= {<Menu/>}/>
+        
+             <Route path="/about" element= {<AboutUs />}/>
+             <Route path="/contect" element= {<Contect/>}/>
+             <Route path="/" element= {<Home/>}/>
+           
+             </Routes>
+             <Footer/>
+      </BrowserRouter> 
+   
+      </>   
+ 
   );
 }
 
